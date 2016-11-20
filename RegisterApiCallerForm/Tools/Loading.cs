e3e21ffcbf8fa8757f6d7f228811f1af3c147330
soheil <soheillamso@gmail.com>
2016-11-20 11:52:04 +0330

    time taken changed

--- RegisterApiCallerForm/Tools/Loading.cs
+++ RegisterApiCallerForm/Tools/Loading.cs
@@ -40,11 +40,12 @@ namespace RegisterApiCallerForm
 
         private void btnDgvBind_Click(object sender, EventArgs e)
         {
-             dataGridView1.Columns.Add("time","time");
-             foreach (var item in new SemnanEntities3().TimeTakens.Select(x => x.time_taken))
-             {
-                 dataGridView1.Rows.Add(item.Value);
-             }
+             //dataGridView1.Columns.Add("time","time");
+             //foreach (var item in new SemnanEntities3().TimeTakens.Select(x => x.time_taken))
+             //{
+             //    dataGridView1.Rows.Add(item.Value);
+             //}
+            dataGridView1.DataSource = new SemnanEntities3().TimeTakens.Select(x => x).ToList();
 
         }
     }
