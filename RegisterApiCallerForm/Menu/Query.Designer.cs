2b2382c64e469ff9144b01165d596b27de5fbb7b
project <project@isetad.ir>
2016-12-13 16:29:47 +0330

    jj

--- RegisterApiCallerForm/Menu/Query.Designer.cs
+++ RegisterApiCallerForm/Menu/Query.Designer.cs
@@ -33,6 +33,8 @@
             this.dgv = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.lblCount = new System.Windows.Forms.Label();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnExcell = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,11 +48,11 @@
             // 
             // btnQuery
             // 
-            this.btnQuery.Location = new System.Drawing.Point(460, 133);
+            this.btnQuery.Location = new System.Drawing.Point(712, 133);
             this.btnQuery.Name = "btnQuery";
             this.btnQuery.Size = new System.Drawing.Size(75, 23);
             this.btnQuery.TabIndex = 1;
-            this.btnQuery.Text = "جستجو";
+            this.btnQuery.Text = "اجرا و نمایش";
             this.btnQuery.UseVisualStyleBackColor = true;
             this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
             // 
@@ -75,16 +77,38 @@
             // lblCount
             // 
             this.lblCount.AutoSize = true;
-            this.lblCount.Location = new System.Drawing.Point(937, 479);
+            this.lblCount.Location = new System.Drawing.Point(903, 478);
             this.lblCount.Name = "lblCount";
             this.lblCount.Size = new System.Drawing.Size(30, 13);
             this.lblCount.TabIndex = 4;
             this.lblCount.Text = "تعداد";
             // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(608, 133);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 5;
+            this.btnExecute.Text = "اجرا";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnExcell
+            // 
+            this.btnExcell.Location = new System.Drawing.Point(820, 474);
+            this.btnExcell.Name = "btnExcell";
+            this.btnExcell.Size = new System.Drawing.Size(48, 23);
+            this.btnExcell.TabIndex = 6;
+            this.btnExcell.Text = "اکسل";
+            this.btnExcell.UseVisualStyleBackColor = true;
+            this.btnExcell.Click += new System.EventHandler(this.btnExcell_Click);
+            // 
             // Query
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnExcell);
+            this.Controls.Add(this.btnExecute);
             this.Controls.Add(this.lblCount);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dgv);
@@ -105,5 +129,7 @@
         private System.Windows.Forms.DataGridView dgv;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnExcell;
     }
 }
--- RegisterApiCallerForm/Menu/Query.cs
+++ RegisterApiCallerForm/Menu/Query.cs
@@ -21,21 +21,28 @@ namespace RegisterApiCallerForm
 
         private void btnQuery_Click(object sender, EventArgs e)
         {
-            string query = "drop table TableQuery; " +
+            string query = "IF OBJECT_ID('dbo.TableQuery', 'U') IS NOT NULL drop table TableQuery; " +
 "select * " +
 "into TableQuery from( ";
             query += tbQuery.Text;
             query += " ) rez2 ;";// +
                 //" ALTER TABLE TableQuery ADD Table_ID int NOT NULL IDENTITY (1,1) PRIMARY KEY;";
-            Publics.ExecuteQuery(new SemnanEntities3(), query);
-            SqlConnection con = new SqlConnection("Data Source=localhost;Initial Catalog=Semnan;User ID=sa;Password=123@abc");
-            SqlCommand com = new SqlCommand("select * from TableQuery", con);
-            SqlDataAdapter data = new SqlDataAdapter(com);
-            DataSet ds=new DataSet();
-            data.Fill(ds);
-            DataTable dt = ds.Tables[0];
-            dgv.DataSource = dt;
-            lblCount.Text = dgv.Rows.Count.ToString();
+            try
+            {
+                Publics.ExecuteQuery(new SemnanEntities3(), query);
+                SqlConnection con = new SqlConnection("Data Source=localhost;Initial Catalog=Semnan;User ID=sa;Password=123@abc");
+                SqlCommand com = new SqlCommand("select * from TableQuery", con);
+                SqlDataAdapter data = new SqlDataAdapter(com);
+                DataSet ds = new DataSet();
+                data.Fill(ds);
+                DataTable dt = ds.Tables[0];
+                dgv.DataSource = dt;
+                lblCount.Text = dgv.Rows.Count.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
             
             
         }
@@ -47,6 +54,16 @@ namespace RegisterApiCallerForm
 	   " (SELECT CAST(time_taken AS int) AS sec, COUNT(CAST(time_taken AS int)) AS cont, CAST(time_taken AS int) * COUNT(CAST(time_taken AS int)) AS multi "+
                            " FROM            dbo.TimeTaken "+
                            " GROUP BY CAST(time_taken AS int)) AS rez ";
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            Publics.ExecuteQuery(new SemnanEntities3(), tbQuery.Text);
+        }
+
+        private void btnExcell_Click(object sender, EventArgs e)
+        {
+            Publics.ExcelMake.ExportToExcell(dgv, 2000);
         }
     }
 }
